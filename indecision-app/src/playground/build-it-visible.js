2b77eee1bb1d7644eedd8393cdaf0720261fc9a7
Jace Babin <jbabin91@gmail.com>
2017-12-12 15:55:14 -0700

    Made updates to build-it-visible, counter-example, and indecision app to use state

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,21 +1,30 @@
-let toggle = false
-
-const onToggle = () => {
-  toggle = !toggle
-  render()
+class VisibilityToggle extends React.Component {
+  constructor (props) {
+    super(props)
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+    this.state = {
+      visibility: false
+    }
+  }
+  handleToggleVisibility () {
+    this.setState(prevState => {
+      return {
+        visibility: !prevState.visibility
+      }
+    })
+  }
+  render () {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? 'Hide Details' : 'Show Details'}
+        </button>
+        {this.state.visibility &&
+          <div><p>Hey. These are some details you can now see!</p></div>}
+      </div>
+    )
+  }
 }
 
-const render = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={onToggle}>
-        {toggle ? 'Hide Details' : 'Show Details'}
-      </button>
-      {toggle && <div><p>Hey. These are some details you can now see!</p></div>}
-    </div>
-  )
-  ReactDOM.render(template, document.getElementById('app'))
-}
-
-render()
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'))
