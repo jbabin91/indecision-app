a7e58482b564e39442766e28b78348552312c3ce
Jace Babin <jbabin91@gmail.com>
2018-01-10 21:14:07 -0700

    Started working on styling

--- indecision-app/.eslintrc.js
+++ indecision-app/.eslintrc.js
@@ -1,3 +1,15 @@
 module.exports = {
-    "extends": "airbnb"
+  extends: ['airbnb'],
+  rules: {
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jxs']
+      }
+    ],
+    'react/prop-types': [0]
+  },
+  globals: {
+    document: true
+  }
 };
